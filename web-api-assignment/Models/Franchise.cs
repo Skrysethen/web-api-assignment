48dbb91cb30468b427f7d13a1166e7153e6d394b
Håvard Lund <havard.lund@no.experis.com>
2023-02-27 14:57:00 +0100

    feat: add movie and franchise model

--- web-api-assignment/Models/Franchise.cs
+++ web-api-assignment/Models/Franchise.cs
@@ -1,7 +1,18 @@
-﻿namespace web_api_assignment.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web_api_assignment.Models
 {
+    [Table("Franchise")]
     public class Franchise
     {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public ICollection<Movie> Movies { get; set; } = null!;
 
     }
 }
