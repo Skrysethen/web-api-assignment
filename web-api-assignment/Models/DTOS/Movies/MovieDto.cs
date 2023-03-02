af1ec607844ae8b72db7d0f745c7e20bd26c5f3e
Håvard Lund <havard.lund@no.experis.com>
2023-03-02 10:30:55 +0100

    feat: create profiles for franchise and movie, also updated characterProfile to include pu and post

--- web-api-assignment/Models/DTOS/Movies/MovieDto.cs
+++ web-api-assignment/Models/DTOS/Movies/MovieDto.cs
@@ -3,7 +3,7 @@ using web_api_assignment.Models.Entities;
 
 namespace web_api_assignment.Models.DTOS.Movies
 {
-    public class MovieDTO
+    public class MovieDto
     {
         public int Id { get; set; }
         [MaxLength(50)]
